1d5d38cc49fc408f89f7359341dabdebd70ddf35
Antony Hernandez <wessoman@hotmail.com>
2019-08-16 17:01:20 -0500

    App begin ver 01.01a

---
+++ ReactGame/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component }from 'react';
+import BoxCard from "./components/BoxCard";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import chars from "./chars.json";
+import "./App.css";
+
