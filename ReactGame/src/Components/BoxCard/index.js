a997394914be22b62c891a015fe041ea63277992
Antony Hernandez <wessoman@hotmail.com>
2019-08-17 11:58:32 -0500

    Ver .24 Component coding build 2

---
+++ ReactGame/src/Components/BoxCard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function BoxCard(props){
+    return <div className="card">
+        <div onClick={() => props.handleClick(props.id, props.name)} className="img-container">
+            <img 
+                id={props.id}
+                src={props.image} 
+                alt={props.name} 
+                isclicked={props.isClicked}
+            />
+        </div>
+    </div>
+}
+
+export default BoxCard;
