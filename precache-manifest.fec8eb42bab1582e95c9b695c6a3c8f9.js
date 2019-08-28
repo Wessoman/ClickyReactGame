7efe1c15f96599a471a430a2aee7197e1e62e5ab
Antony Hernandez <wessoman@hotmail.com>
2019-08-28 13:24:54 -0500

    Updates

---
+++ precache-manifest.fec8eb42bab1582e95c9b695c6a3c8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b3fec33f2b1f684afe07a880557d10",
+    "url": "/ClickyReactGame/index.html"
+  },
+  {
+    "revision": "dec67a270a013e63388e",
+    "url": "/ClickyReactGame/static/css/main.4a5a18d3.chunk.css"
+  },
+  {
+    "revision": "312e0fcd832fe92820bb",
+    "url": "/ClickyReactGame/static/js/2.9452b0d2.chunk.js"
+  },
+  {
+    "revision": "dec67a270a013e63388e",
+    "url": "/ClickyReactGame/static/js/main.22bb6661.chunk.js"
+  },
+  {
+    "revision": "4d598dc6abfb42a6de53",
+    "url": "/ClickyReactGame/static/js/runtime~main.d2f2a484.js"
+  },
+  {
+    "revision": "08798699eff17594d5b14259fa51239a",
+    "url": "/ClickyReactGame/static/media/Neo Gen.08798699.ttf"
+  }
+]);
