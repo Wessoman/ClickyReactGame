a99f5f1a5ce1921b543aaa2ba06c08f5f62e1fe8
Antony Hernandez <wessoman@hotmail.com>
2019-08-28 13:10:10 -0500

    Ver .33 Filestructure

---
+++ build/precache-manifest.ba31f158bcee6293538bbd5ad42d2fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6fbf80ce2df8e0544719a67fa4478a",
+    "url": "/ClickyReactGame/index.html"
+  },
+  {
+    "revision": "4c54356a93030ae154a6",
+    "url": "/ClickyReactGame/static/css/main.3786f1fe.chunk.css"
+  },
+  {
+    "revision": "312e0fcd832fe92820bb",
+    "url": "/ClickyReactGame/static/js/2.9452b0d2.chunk.js"
+  },
+  {
+    "revision": "4c54356a93030ae154a6",
+    "url": "/ClickyReactGame/static/js/main.bcece510.chunk.js"
+  },
+  {
+    "revision": "4d598dc6abfb42a6de53",
+    "url": "/ClickyReactGame/static/js/runtime~main.d2f2a484.js"
+  },
+  {
+    "revision": "240f98d60ddfdb46259790c8e81838f9",
+    "url": "/ClickyReactGame/static/media/Amuro.240f98d6.otf"
+  }
+]);
